9fe095d28ecaf3dfdfdfce1e2de7b71159715c6a
plushyday <plushik.n@gmail.com>
2016-10-15 18:09:36 +0200

    Add files via upload

--- task-list-controller.js
+++ task-list-controller.js
@@ -1,19 +1,24 @@
+/*
 angular.module('taskManager').component('taskListController', {
-	templateUrl: '/task-list-controller.html',
+	templateUrl: 'task-list-controller.html',
 	controller: ['$scope', function($scope){
-		this.currentSortAscending = false;
-		this.currentSortProperty = null; 
-		$scope.toggleSort = function(property){
-			if(this.currentSortProperty == property)
-				this.currentSortAscending = !this.currentSortAscending;
-			else{
-				this.currentSortProperty = property;
-				this.currentSortAscending = false;
-			}
-		}
+		
 
 	}],
 	bindings: {
 		task: '='
 	}
 });
+*/
+angular.module('taskManager').controller('taskListController', ['$scope', function($scope){
+    this.currentSortAscending = false;
+	this.currentSortProperty = null; 
+	this.toggleSort = function(property){
+		if(this.currentSortProperty == property)
+			this.currentSortAscending = !this.currentSortAscending;
+		else{
+			this.currentSortProperty = property;
+			this.currentSortAscending = false;
+		}
+	} 
+}])
